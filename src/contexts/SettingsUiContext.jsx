51dd88e0d4d693e6c430c30a866cc5b1c289dec3
neha <nehashah.0399@gmail.com>
2026-04-23 13:13:09 +0530

    Removed comments

--- src/contexts/SettingsUiContext.jsx
+++ src/contexts/SettingsUiContext.jsx
@@ -1,38 +1,3 @@
- 
-// import React, { createContext, useCallback, useContext, useMemo, useState } from "react";
-// import SettingsDrawer from "../components/SettingsDrawer";
- 
-// const SettingsUiContext = createContext({
-//   settingsOpen: false,
-//   openSettings: () => {},
-//   closeSettings: () => {},
-//   toggleSettings: () => {},
-// });
- 
-// export function SettingsUiProvider({ children }) {
-//   const [settingsOpen, setSettingsOpen] = useState(false);
- 
-//   const openSettings = useCallback(() => setSettingsOpen(true), []);
-//   const closeSettings = useCallback(() => setSettingsOpen(false), []);
-//   const toggleSettings = useCallback(() => setSettingsOpen((o) => !o), []);
- 
-//   const value = useMemo(
-//     () => ({ settingsOpen, openSettings, closeSettings, toggleSettings }),
-//     [settingsOpen, openSettings, closeSettings, toggleSettings],
-//   );
- 
-//   return (
-//     <SettingsUiContext.Provider value={value}>
-//       {children}
-//       <SettingsDrawer open={settingsOpen} onClose={closeSettings} />
-//     </SettingsUiContext.Provider>
-//   );
-// }
- 
-// export function useSettingsUi() {
-//   return useContext(SettingsUiContext);
-// }
- 
 import React, { createContext, useContext, useMemo, useState } from "react";
 import SettingsDrawer from "../components/SettingsDrawer";
 
